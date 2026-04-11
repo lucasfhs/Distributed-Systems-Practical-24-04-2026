125364df905646ca0d253a11d07c67d27375718f
lucashfsdev <lucashfsdev@gmail.com>
2026-04-11 06:29:17 -0300

    git ignore ajustado.

--- project/src/threads/utils.hpp
+++ project/src/threads/utils.hpp
@@ -4,7 +4,7 @@
 template <size_t N>
 int get_buffer_size(const std::array<int, N>& shared_memory) {
     int count = 0;
-    for (int i = 0; i < N; i++) {
+    for (size_t i = 0; i < N; i++) {
         if (shared_memory[i] != 0) count++;
     }
     return count;
